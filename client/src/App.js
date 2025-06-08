4a92f3b68bfd4c1a476a8821f029eb276e366adb
Lukasz <urbanskiluk2001@gmail.com>
2025-06-08 18:05:10 +0200

    Final version after refactor and comments cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />  {/* Domyślna strona to logowanie */}
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
@@ -35,32 +35,3 @@ function App() {
 }
 
 export default App;
-
-
-//Główny komponent aplikacji React, będący punktem centralnym logiki interfejsu użytkownika.
-
-//Co się dzieje:
-//Importuje React, hook useState oraz Axios (do wykonywania zapytań HTTP).
-//Definiuje stan books, który przechowuje listę książek pobranych z backendu.
-//Funkcja fetchBooks – wywoływana przy kliknięciu przycisku "Wyświetl książki". Wykonuje zapytanie do API (http://localhost:5000/api/books) i aktualizuje stan.
-//Renderuje interfejs użytkownika: tytuł, przycisk do pobrania książek oraz komponent BookList, który wyświetla otrzymane dane.
-
-
-// Cel:
-// Główny komponent aplikacji front-end (React), który definiuje trasowanie (routing) oraz określa, którą stronę renderować w zależności od adresu URL.
-
-// Szczegółowy przebieg działania:
-
-// Importy:
-// Importujesz elementy z react-router-dom (BrowserRouter, Routes, Route) oraz komponenty stron: Login, Register, Home.
-
-// Konfiguracja Routera:
-// Wewnątrz komponentu App otaczasz całą aplikację routerem (Router). Następnie definiujesz zestaw tras (Routes):
-
-// Domyślna strona (path="/") – wyświetla logowanie.
-// Strona /login – komponent Login.
-// Strona /register – komponent Register.
-// Strona /home – strona, na którą przekierowuje się po zalogowaniu (Home).
-
-// Przekazywanie elementów:
-// Każdy Route wskazuje, który komponent ma być wyrenderowany w danej ścieżce, dzięki czemu użytkownik po zmianie adresu widzi odpowiednią stronę.
